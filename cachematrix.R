e4ff9fa4509c1d3105c946282f240c41c09d5c4b
Sean Hill <sean@reax.io>
2014-12-16 12:39:22 -0600

    Add function to build matrix with cacheable solve (inverse)

--- cachematrix.R
+++ cachematrix.R
@@ -2,10 +2,22 @@
 ## Use the makeCacheMatrix to return a cacheable matrix and then use
 ## cacheSolve to compute the inverse and cache it or return the cached inverse
 
-## Write a short comment describing this function
+## This function takes a matrix and returns a list of functions for caching its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  get <- function() x
+  
+  set_solve <- function(solve) s <<- solve
+  get_solve <- function() s
+  
+  list(set = set, get = get, set_solve = set_solve, get_solve = get_solve)
 }
 
 
